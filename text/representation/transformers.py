47e992433bffcedcf6974190acfab61ad1a05d2d
Nisarg Shah <snisarg@gmail.com>
2020-12-09 09:17:52 -0800

    Added missing class

--- text/representation/transformers.py
+++ text/representation/transformers.py
@@ -1,8 +1,45 @@
-from typing import Optional
+from typing import Optional, List
 
 from torch import nn
+
+from text.decoder.mlp import GeLU
 from text.representation.attention import MultiheadSelfAttention
-from text.representation.mlp import ResidualMLP
+
+
+class ResidualMLP(nn.Module):
+    """A square MLP component which can learn a bias on an input vector.
+    This MLP in particular defaults to using GeLU as its activation function
+    (this can be changed by passing a different activation function),
+    and retains a residual connection to its original input to help with gradient
+    propogation.
+
+    Unlike pytext's MLPDecoder it doesn't currently allow adding a LayerNorm
+    in between hidden layers.
+    """
+
+    def __init__(
+        self,
+        input_dim: int,
+        hidden_dims: List[int],
+        dropout: float = 0.1,
+        activation=GeLU,
+    ):
+        super().__init__()
+        modules = []
+        for last_dim, dim in zip([input_dim] + hidden_dims, hidden_dims):
+            modules.extend(
+                [nn.Linear(last_dim, dim), activation(), nn.Dropout(dropout)]
+            )
+
+        last_dim = hidden_dims[-1] if hidden_dims else input_dim
+        # Unlike normal PyText mlp, we don't put an activation layer at the end.
+        modules.extend([nn.Linear(last_dim, input_dim), nn.Dropout(dropout)])
+
+        self.mlp = nn.Sequential(*modules)
+
+    def forward(self, input):
+        bias = self.mlp(input)
+        return input + bias
 
 
 class TransformerLayer(nn.Module):
